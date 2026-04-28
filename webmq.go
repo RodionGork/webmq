5753d03c4ce44963e234926cf846fe4f3466ffb9
Rodion <r.gorkovenko@yadro.com>
2026-04-28 11:17:53 +0300

    Introduced parallel workers, timeout parsed but not used yet

--- webmq.go
+++ webmq.go
@@ -3,15 +3,40 @@
 package main
 
 import (
+	"bytes"
+	"errors"
 	"fmt"
+	"hash/maphash"
 	"net"
 	"net/http"
 	"net/url"
 	"os"
+	"strconv"
+	"time"
 )
 
+const defaultTimeout = 7
+const readBufMax = 2048 // based upon popular limit on URL length (2000 chars)
+var noNewlineError = errors.New("No newline found when parsing http request")
+
 type mqWebServer struct {
-	topics map[string]chan string
+	readBuf  []byte
+	workers  []*worker
+	hashSeed maphash.Seed
+}
+
+type worker struct {
+	ch      chan *request
+	topics  map[string][]string
+	waiting map[string][]*request
+}
+
+type request struct {
+	conn     net.Conn
+	method   string
+	path     string
+	msg      string
+	deadline int64
 }
 
 func (s *mqWebServer) ListenAndServe(port string) {
@@ -23,71 +48,91 @@ func (s *mqWebServer) ListenAndServe(port string) {
 	for {
 		conn, err := listener.Accept()
 		if err == nil {
-			method, path, query, err := reqParse(conn)
+			req, err := s.reqParse(conn)
 			if err == nil {
-				s.process(conn, method, path, query)
+				h := int(maphash.String(s.hashSeed, req.path) & 0xFFFFFFFF)
+				s.workers[h%len(s.workers)].ch <- req
 			} else {
 				println("error on parsing request:", err.Error())
+				conn.Close()
 			}
-			conn.Close()
 		} else {
 			println("error on accept:", err.Error())
 		}
 	}
 }
 
-func (s *mqWebServer) process(conn net.Conn, method, path string, query url.Values) {
-	msg := query.Get("v")
-	switch method {
-	case "PUT":
-		if msg == "" {
-			respWrite(conn, http.StatusBadRequest, "")
-			return
+func (w *worker) work() {
+	for {
+		select {
+		case req := <-w.ch:
+			w.process(req)
+		default:
+			time.Sleep(30 * time.Millisecond)
 		}
-		c, ok := s.topics[path]
-		if !ok {
-			c = make(chan string, 16)
-			s.topics[path] = c
-		}
-		c <- msg
-		respWrite(conn, http.StatusOK, "")
-	case "GET":
-		msg := ""
-		c := s.topics[path]
-		if c != nil {
-			select {
-			case m := <-c:
-				msg = m
-			default:
-			}
-		}
-		if msg != "" {
-			respWrite(conn, http.StatusOK, msg)
-		} else {
-			respWrite(conn, http.StatusNotFound, "")
-		}
-	default:
-		respWrite(conn, http.StatusMethodNotAllowed, "")
 	}
 }
 
-func reqParse(conn net.Conn) (string, string, url.Values, error) {
-	b := make([]byte, 1024)
-	n, err := conn.Read(b)
-	if err != nil {
-		return "", "", nil, err
+func (w *worker) process(req *request) {
+	switch req.method {
+	case "PUT":
+		if req.msg == "" {
+			respWrite(req.conn, http.StatusBadRequest, "")
+			return
+		}
+		q, ok := w.topics[req.path]
+		if !ok {
+			w.topics[req.path] = []string{req.msg}
+		}
+		w.topics[req.path] = append(q, req.msg)
+		respWrite(req.conn, http.StatusOK, "")
+	case "GET":
+		q := w.topics[req.path]
+		if q != nil {
+			if len(q) > 0 {
+				w.topics[req.path] = q[1:]
+				respWrite(req.conn, http.StatusOK, q[0]+"\n")
+				return
+			}
+			delete(w.topics, req.path)
+		}
+		respWrite(req.conn, http.StatusNotFound, "")
+	default:
+		respWrite(req.conn, http.StatusMethodNotAllowed, "")
 	}
-	b = b[:n]
+}
+
+func (s *mqWebServer) reqParse(conn net.Conn) (*request, error) {
+	n, err := conn.Read(s.readBuf)
+	if err != nil {
+		return nil, err
+	}
+	crpos := bytes.IndexByte(s.readBuf[:n], '\n')
+	if crpos == -1 {
+		return nil, noNewlineError
+	}
+	data := s.readBuf[:n]
 	var method, path, httpVer string
-	fmt.Sscanf(string(b), "%s %s %s", &method, &path, &httpVer)
+	fmt.Sscanf(string(data), "%s %s %s", &method, &path, &httpVer)
 	if len(path) > 0 && path[0] == '/' {
 		path = path[1:]
 	}
 	u, err := url.Parse(path)
 	if err != nil {
-		return "", "", nil, err
+		return nil, err
 	}
-	return method, u.Path, u.Query(), nil
+	to, err := strconv.Atoi(u.Query().Get("timeout"))
+	if err != nil {
+		to = defaultTimeout
+	}
+	req := &request{
+		conn:     conn,
+		method:   method,
+		path:     u.Path,
+		msg:      u.Query().Get("v"),
+		deadline: time.Now().UnixMilli() + int64(to)*1000,
+	}
+	return req, nil
 }
 
 func respWrite(conn net.Conn, status int, data string) {
@@ -96,17 +141,39 @@ func respWrite(conn net.Conn, status int, data string) {
 	text += fmt.Sprintf("Content-Length: %d\r\n\r\n", len(b))
 	conn.Write([]byte(text))
 	conn.Write(b)
+	conn.Close()
 }
 
 func newMqWebServer() *mqWebServer {
-	return &mqWebServer{topics: map[string]chan string{}}
+	numWorkers, err := strconv.Atoi(os.Getenv("NUM_WORKERS"))
+	if err != nil {
+		println("use NUM_WORKERS env variable to set number of workers")
+		numWorkers = 1
+	}
+	workers := make([]*worker, numWorkers)
+	for i := 0; i < numWorkers; i++ {
+		workers[i] = &worker{
+			ch:      make(chan *request),
+			topics:  map[string][]string{},
+			waiting: map[string][]*request{},
+		}
+		go workers[i].work()
+	}
+	return &mqWebServer{
+		readBuf:  make([]byte, readBufMax),
+		hashSeed: maphash.MakeSeed(),
+		workers:  workers,
+	}
 }
 
 func main() {
 	port := "8000"
 	if len(os.Args) > 1 {
 		port = os.Args[1]
+	} else {
+		println("To change the default port set it as a first command line parameter")
 	}
 	srv := newMqWebServer()
+	println("Starting at port =", port, "with num workers = ", len(srv.workers))
 	srv.ListenAndServe(":" + port)
 }
